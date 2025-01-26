13b9a41abb3e886bbc1aaeb9c27523c76569d4ca
Hamad8-8 <assassinsknight@hotmail.com>
2025-01-26 16:02:38 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  //Edit made by Student-X
+  //Edit made by Student - HamadAfzal_230123308
 
   //Last line print('TA TA')
 }
